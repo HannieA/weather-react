7aa742b8f6d6ee40851cc915f1fffb562327d50a
HannieA <ann.986@gmail.com>
2022-09-05 12:59:56 -0700

    Change link to github repository

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/HannieA/weather-app">Open source code </a>
+      <a href="https://github.com/HannieA/weather-react">Open source code </a>
       by Hanna Anishyna
     </div>
   );
