698079f1c27d04920c7d9b4148d467ea15f926c5
HannieA <ann.986@gmail.com>
2022-09-16 17:29:19 -0700

    Add Forecast component

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast() {
-  return <section id="forecast" />;
+  return (
+    <section classNameName="forecast">
+      <div classNameName="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <ul>
+                <li className="day-forecast">Tue</li>
+                <li>
+                  <img className="icon-forecast" />
+                </li>
+                <li>
+                  {" "}
+                  <span className="temperature-max">10</span>° |{" "}
+                  <span className="temperature-min">6</span>°{" "}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
+    </section>
+  );
 }
